7b0631e45e2cbfbdbe94a301f5198fa5de42d0d0
Jeremy Maitin-Shepard <jbms@google.com>
2020-10-27 10:48:04 -0700

    Fix JsonSame to handle nested `discarded` values, and to be non-recurisve
    Nested discard handling will be needed by a subsequent change that
    adds JSON Pointer handling.
    
    PiperOrigin-RevId: 339282375
    Change-Id: I8052307f3cb5c0d880198f7c41aca591b1c06aa8

--- tensorstore/internal/json.cc
+++ tensorstore/internal/json.cc
@@ -21,8 +21,10 @@
 #include <optional>
 #include <string>
 #include <utility>
+#include <variant>
 #include <vector>
 
+#include "absl/container/inlined_vector.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
@@ -38,9 +40,78 @@ namespace tensorstore {
 namespace internal_json {
 
 bool JsonSame(const ::nlohmann::json& a, const ::nlohmann::json& b) {
-  if (a == b) return true;
-  if (a.is_discarded() && b.is_discarded()) return true;
-  return false;
+  using value_t = ::nlohmann::json::value_t;
+  using array_t = ::nlohmann::json::array_t;
+  using object_t = ::nlohmann::json::object_t;
+  struct ArrayIterators {
+    array_t::const_iterator a_cur, a_end, b_cur;
+  };
+  struct ObjectIterators {
+    object_t::const_iterator a_cur, a_end, b_cur;
+  };
+  using StackEntry = std::variant<ArrayIterators, ObjectIterators>;
+  absl::InlinedVector<StackEntry, 64> stack;
+  const auto compare_or_defer_values = [&](const ::nlohmann::json& a_value,
+                                           const ::nlohmann::json& b_value) {
+    const auto t = a_value.type();
+    switch (t) {
+      case value_t::discarded:
+      case value_t::null:
+        return b_value.type() == t;
+      case value_t::array: {
+        if (b_value.type() != t) return false;
+        const auto& a_arr = a_value.get_ref<const array_t&>();
+        const auto& b_arr = b_value.get_ref<const array_t&>();
+        if (a_arr.size() != b_arr.size()) return false;
+        if (a_arr.empty()) return true;
+        stack.emplace_back(
+            ArrayIterators{a_arr.begin(), a_arr.end(), b_arr.begin()});
+        return true;
+      }
+      case value_t::object: {
+        if (b_value.type() != t) return false;
+        const auto& a_obj = a_value.get_ref<const object_t&>();
+        const auto& b_obj = b_value.get_ref<const object_t&>();
+        if (a_obj.size() != b_obj.size()) return false;
+        if (a_obj.empty()) return true;
+        stack.emplace_back(
+            ObjectIterators{a_obj.begin(), a_obj.end(), b_obj.begin()});
+        return true;
+      }
+      default:
+        return a_value == b_value;
+    }
+  };
+  if (!compare_or_defer_values(a, b)) return false;
+  while (!stack.empty()) {
+    auto& e = stack.back();
+    if (auto* array_iterators = std::get_if<ArrayIterators>(&e)) {
+      auto& a_v = *array_iterators->a_cur;
+      auto& b_v = *array_iterators->b_cur;
+      if (++array_iterators->a_cur == array_iterators->a_end) {
+        stack.pop_back();
+      } else {
+        ++array_iterators->b_cur;
+      }
+      if (!compare_or_defer_values(a_v, b_v)) {
+        return false;
+      }
+    } else {
+      auto* object_iterators = std::get_if<ObjectIterators>(&e);
+      auto& a_kv = *object_iterators->a_cur;
+      auto& b_kv = *object_iterators->b_cur;
+      if (++object_iterators->a_cur == object_iterators->a_end) {
+        stack.pop_back();
+      } else {
+        ++object_iterators->b_cur;
+      }
+      if (a_kv.first != b_kv.first ||
+          !compare_or_defer_values(a_kv.second, b_kv.second)) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 Status ExpectedError(const ::nlohmann::json& j, absl::string_view type_name) {
--- tensorstore/internal/json.h
+++ tensorstore/internal/json.h
@@ -44,6 +44,12 @@ namespace internal_json {
 
 /// Returns `true` if `a` and `b` are equal.
 ///
+/// Unlike `operator==`, the comparison is non-recursive, and is therefore safe
+/// from stack overflow even for deeply nested structures.
+///
+/// Like `operator==`, two int64_t/uint64_t/double values representing exactly
+/// the same number are all considered equal even if their types differ.
+///
 /// Unlike `operator==`, two `discarded` values are considered equal.
 bool JsonSame(const ::nlohmann::json& a, const ::nlohmann::json& b);
 
