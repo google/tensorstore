9f809b46e8f453a1d3eac35f9e7aa7176b9ce70b
Jeremy Maitin-Shepard <jbms@google.com>
2021-03-06 06:37:05 -0800

    Add SmallBitSet (and DimensionSet alias)
    PiperOrigin-RevId: 361318852
    Change-Id: I937ee95e8ff47b9ed64de3d8675aefb53c2401a1

---
+++ tensorstore/util/small_bit_set.h
@@ -0,0 +1,128 @@
+// Copyright 2021 The TensorStore Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TENSORSTORE_UTIL_SMALL_BIT_SET_H_
+#define TENSORSTORE_UTIL_SMALL_BIT_SET_H_
+
+#include <cstdint>
+#include <ostream>
+
+#include "tensorstore/internal/integer_types.h"
+#include "tensorstore/util/bit_span.h"
+
+namespace tensorstore {
+
+/// Bit set that fits in a single unsigned integer.
+///
+/// This is similar to `std::bitset<N>`, but supports iterators, and for
+/// simplicity is limited to a single unsigned integer.
+template <size_t N>
+class SmallBitSet {
+ public:
+  using Bits = typename internal::uint_type<N>::type;
+
+  using value_type = bool;
+  using reference = BitRef<Bits>;
+  using const_reference = BitRef<const Bits>;
+  using iterator = BitIterator<Bits>;
+  using const_iterator = BitIterator<const Bits>;
+  using size_type = size_t;
+  using difference_type = size_t;
+
+  /// Constructs an all-zero vector.
+  constexpr SmallBitSet() : bits_(0) {}
+
+  /// Constructs a vector with all bits set to `value`.
+  constexpr SmallBitSet(bool value) : bits_(value * ~Bits(0)) {}
+
+  /// Constructs a vector from an unsigned integer.
+  static constexpr SmallBitSet FromBits(Bits bits) {
+    SmallBitSet v;
+    v.bits_ = bits;
+    return v;
+  }
+
+  /// Sets all bits to the specified value.
+  constexpr SmallBitSet& operator=(bool value) {
+    bits_ = ~Bits(0) * value;
+    return *this;
+  }
+
+  constexpr iterator begin() { return iterator(&bits_, 0); }
+  constexpr iterator end() { return iterator(&bits_, N); }
+  constexpr const_iterator begin() const { return const_iterator(&bits_, 0); }
+  constexpr const_iterator end() const { return const_iterator(&bits_, N); }
+
+  constexpr size_t size() const { return N; }
+
+  constexpr reference operator[](size_t offset) {
+    assert(offset >= 0 && offset < N);
+    return reference(&bits_, offset);
+  }
+  constexpr const_reference operator[](size_t offset) const {
+    assert(offset >= 0 && offset < N);
+    return const_reference(&bits_, offset);
+  }
+
+  /// Returns `true` if any bit is set.
+  explicit operator bool() const { return static_cast<bool>(bits_); }
+
+  friend constexpr SmallBitSet operator~(SmallBitSet v) {
+    return SmallBitSet::FromBits(~v.bits_);
+  }
+  friend constexpr SmallBitSet operator&(SmallBitSet a, SmallBitSet b) {
+    return SmallBitSet::FromBits(a.bits_ & b.bits_);
+  }
+  friend constexpr SmallBitSet operator^(SmallBitSet a, SmallBitSet b) {
+    return SmallBitSet::FromBits(a.bits_ ^ b.bits_);
+  }
+  friend constexpr SmallBitSet operator|(SmallBitSet a, SmallBitSet b) {
+    return SmallBitSet::FromBits(a.bits_ | b.bits_);
+  }
+  friend constexpr SmallBitSet& operator&=(SmallBitSet& a, SmallBitSet b) {
+    a.bits_ &= b.bits_;
+    return a;
+  }
+  friend constexpr SmallBitSet& operator^=(SmallBitSet& a, SmallBitSet b) {
+    a.bits_ ^= b.bits_;
+    return a;
+  }
+  friend constexpr SmallBitSet& operator|=(SmallBitSet& a, SmallBitSet b) {
+    a.bits_ |= b.bits_;
+    return a;
+  }
+  friend constexpr bool operator==(SmallBitSet a, SmallBitSet b) {
+    return a.bits_ == b.bits_;
+  }
+  friend constexpr bool operator!=(SmallBitSet a, SmallBitSet b) {
+    return !(a == b);
+  }
+
+  /// Returns the bitvector as an unsigned integer.
+  Bits bits() const { return bits_; }
+
+  friend std::ostream& operator<<(std::ostream& os, SmallBitSet v) {
+    for (size_t i = 0; i < N; ++i) {
+      os << (static_cast<bool>(v[i]) ? '1' : '0');
+    }
+    return os;
+  }
+
+ private:
+  Bits bits_;
+};
+
+}  // namespace tensorstore
+
+#endif  // TENSORSTORE_UTIL_SMALL_BIT_SET_H_
---
+++ tensorstore/util/small_bit_set_test.cc
@@ -0,0 +1,82 @@
+// Copyright 2021 The TensorStore Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tensorstore/util/small_bit_set.h"
+
+#include <vector>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "tensorstore/util/str_cat.h"
+
+namespace {
+
+using BitSet = tensorstore::SmallBitSet<32>;
+
+TEST(SmallBitSetTest, DefaultConstruct) {
+  BitSet v;
+  EXPECT_FALSE(v);
+  EXPECT_EQ(0, v.bits());
+  EXPECT_EQ(v, v);
+  BitSet v_true = true;
+  EXPECT_EQ(v_true, v_true);
+  EXPECT_NE(v, v_true);
+  EXPECT_THAT(v, ::testing::ElementsAreArray(std::vector<bool>(32)));
+}
+
+TEST(SmallBitSetTest, FromBits) {
+  auto v = BitSet::FromBits(0b11'0111);
+  EXPECT_TRUE(v);
+  EXPECT_EQ(0b110111, v.bits());
+  EXPECT_EQ(true, v[0]);
+  EXPECT_EQ(true, v[1]);
+  EXPECT_EQ(true, v[2]);
+  EXPECT_EQ(false, v[3]);
+  EXPECT_EQ(true, v[4]);
+  EXPECT_EQ(true, v[5]);
+  EXPECT_THAT(v, ::testing::ElementsAre(1, 1, 1, 0, 1, 1, 0, 0,  //
+                                        0, 0, 0, 0, 0, 0, 0, 0,  //
+                                        0, 0, 0, 0, 0, 0, 0, 0,  //
+                                        0, 0, 0, 0, 0, 0, 0, 0));
+  EXPECT_EQ(
+      "11101100"
+      "00000000"
+      "00000000"
+      "00000000",
+      tensorstore::StrCat(v));
+  EXPECT_EQ(0b11111111'11111111'11111111'11001000, (~v).bits());
+  auto v1 = BitSet::FromBits(0b101'1100);
+  EXPECT_EQ(0b111'1111, (v | v1).bits());
+  EXPECT_EQ(0b001'0100, (v & v1).bits());
+  EXPECT_EQ(0b110'1011, (v ^ v1).bits());
+  auto v2 = v1;
+  v2 |= v;
+  EXPECT_EQ(0b111'1111, v2.bits());
+  v2 = v1;
+  v2 &= v;
+  EXPECT_EQ(0b001'0100, v2.bits());
+  v2 = v1;
+  v2 ^= v;
+  EXPECT_EQ(0b110'1011, v2.bits());
+}
+
+TEST(SmallBitSetTest, Reference) {
+  BitSet v;
+  v[2] = true;
+  EXPECT_TRUE(v[2]);
+  EXPECT_FALSE(v[0]);
+  EXPECT_EQ(0b100, v.bits());
+}
+
+}  // namespace
