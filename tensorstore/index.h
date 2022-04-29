dde29cf32756f0de3e8e2d8d6ef334e1efbfeecb
Jeremy Maitin-Shepard <jbms@google.com>
2022-04-29 10:42:53 -0700

    Update index.h for C++ API documentation
    PiperOrigin-RevId: 445455419
    Change-Id: Ic4033492fd9b909342cc49b939dfce5377e2d7f5

--- tensorstore/index.h
+++ tensorstore/index.h
@@ -23,27 +23,54 @@ namespace tensorstore {
 
 /// Type for representing a coordinate in, or a extent of, a multi-dimensional
 /// array.
+///
+/// A valid index is in the closed interval: [`kMinFiniteIndex`,
+/// `kMaxFiniteIndex`].
+///
+/// \ingroup indexing
 using Index = std::int64_t;
 
 /// Type for representing a dimension index or rank.
+///
+/// \ingroup indexing
 using DimensionIndex = std::ptrdiff_t;
 
-/// A valid index is in the closed interval:
-/// `[kMinFiniteIndex, kMaxFiniteIndex]`.
+/// Minimum supported finite `Index` value.
+///
+/// \relates Index
+constexpr Index kMinFiniteIndex = -0x3ffffffffffffffe;  // -(2^62-2)
 
+/// Special `Index` value representing positive infinity.
+///
 /// The special value of `-kInfIndex` when used as a lower bound indicates an
 /// index range that is unbounded below, and the special value of `+kInfIndex`
 /// when used as an upper bound indicates an index range that is unbounded
 /// above.
-constexpr Index kMinFiniteIndex = -0x3ffffffffffffffe;  // -(2^62-2)
+///
+/// \relates Index
 constexpr Index kInfIndex = 0x3fffffffffffffff;         // 2^62-1
+
+/// Maximum supported finite `Index` value.
+///
+/// \relates Index
 constexpr Index kMaxFiniteIndex = 0x3ffffffffffffffe;   // 2^62-2
+
+/// Special `Index` value representing an infinite extent for a dimension.
+///
+/// \relates Index
 constexpr Index kInfSize = 0x7fffffffffffffff;          // 2^63-1
+
+/// Maximum supported finite `Index` value.
+///
+/// \relates Index
 constexpr Index kMaxFiniteSize = 0x7ffffffffffffffd;    // 2^63-3
+
 static_assert(-kInfIndex + kInfSize - 1 == kInfIndex, "");
 static_assert(kMinFiniteIndex + kMaxFiniteSize - 1 == kMaxFiniteIndex, "");
 
 /// Special index value that indicates an implicit bound.
+///
+/// \relates Index
 constexpr Index kImplicit = -0x8000000000000000;  // -2^63
 
 }  // namespace tensorstore
