b311db5485a351964aea1d2a1fd703e44986f123
Jeremy Maitin-Shepard <jbms@google.com>
2022-04-29 10:46:25 -0700

    Update rank.h for C++ API documentation
    PiperOrigin-RevId: 445456269
    Change-Id: I95f5cfbb42ae8c2755a47c50e35e7b184e456809

--- tensorstore/rank.h
+++ tensorstore/rank.h
@@ -37,22 +37,29 @@
 namespace tensorstore {
 
 /// Maximum supported rank.
+///
+/// \relates DimensionIndex
 constexpr DimensionIndex kMaxRank = 32;
 
 /// Checks if `rank` is a valid rank value.
+///
+/// \relates DimensionIndex
 constexpr inline bool IsValidRank(DimensionIndex rank) {
   return 0 <= rank && rank <= kMaxRank;
 }
 
+/// Type of the special `dynamic_rank` constant.
+///
+/// \relates dynamic_rank
 struct DynamicRank {
   /// Enables the use of `dynamic_rank` below as a constant argument for
-  /// `DimensionIndex Rank` template parameters to indicate a dynamic rank (with
-  /// inline buffer disabled, if applicable).
+  /// ``DimensionIndex Rank`` template parameters to indicate a dynamic rank
+  /// (with inline buffer disabled, if applicable).
   constexpr operator DimensionIndex() const { return -1; }
 
-  /// Enables the use of `dynamic_rank(n)` as an argument for
-  /// `DimensionIndex Rank` template parameters to indicate a dynamic rank with
-  /// an inline buffer of size `n`.
+  /// Enables the use of ``dynamic_rank(n)`` as an argument for
+  /// ``DimensionIndex Rank`` template parameters to indicate a dynamic rank
+  /// with an inline buffer of size ``n``.
   constexpr DimensionIndex operator()(DimensionIndex inline_buffer_size) const {
     assert(inline_buffer_size >= 0);
     assert(inline_buffer_size <= kMaxRank);
@@ -66,9 +73,11 @@ struct DynamicRank {
 /// which at compile-time represents a rank to be specified at run-time, and at
 /// run-time represents an unknown rank.
 ///
-/// The syntax `dynamic_rank(n)`, for `n >= 0`, is used to indicate a dynamic
-/// rank with an inline buffer that accommodates ranks up to `n`.  Note that
-/// `dynamic_rank` is equivalent to `dynamic_rank(0)`.
+/// The syntax ``dynamic_rank(n)``, for ``n >= 0``, is used to indicate a
+/// dynamic rank with an inline buffer that accommodates ranks up to ``n``.
+/// Note that `dynamic_rank` is equivalent to `dynamic_rank(0)`.
+///
+/// \relates DimensionIndex
 constexpr inline DynamicRank dynamic_rank = {};
 
 /// Specifies a fixed compile-time rank, or a run-time rank with an optional
@@ -77,26 +86,36 @@ constexpr inline DynamicRank dynamic_rank = {};
 /// - Non-negative integers `0`, `1`, ..., `kMaxRank` indicate a fixed rank
 ///   specified at compile time.
 ///
-/// - A value of :cpp:`dynamic_rank(k)` (equal to :cpp:`-k -1`), for
-///   `0 <= k <= kMaxRank`, indicates that the rank is determined at run time,
-///   and bounds/layout information for rank values up to :cpp:`k` will be
+/// - A value of ``dynamic_rank(k)`` (equal to ``-k -1``), for
+///   ``0 <= k <= kMaxRank``, indicates that the rank is determined at run
+///   time, and bounds/layout information for rank values up to ``k`` will be
 ///   stored inline without additional heap allocation.
 ///
 /// - The special value `dynamic_rank`, equivalent to `dynamic_rank(0)`,
 ///   indicates that the rank is determined at run time, and any non-zero rank
 ///   will require heap-allocated storage of bounds/layout information.
+///
+/// \ingroup Indexing
 using InlineRank = DimensionIndex;
 
 /// Represents an optional rank value and provides related operations.
+///
+/// \ingroup Indexing
 struct RankConstraint {
   /// Constructs with an unspecified rank value (`dynamic_rank`).
+  ///
+  /// \id dynamic
   constexpr RankConstraint() = default;
   constexpr RankConstraint(DynamicRank) {}
 
   /// Constructs with the specified rank value.
+  ///
+  /// \id rank
   constexpr explicit RankConstraint(DimensionIndex rank) : rank(rank) {}
 
   /// Constructs from an `InlineRank` value, ignoring the inline buffer size.
+  ///
+  /// \membergroup Constructors
   static constexpr RankConstraint FromInlineRank(InlineRank value) {
     return RankConstraint(value < 0 ? dynamic_rank : value);
   }
@@ -115,6 +134,8 @@ struct RankConstraint {
   /// Returns the intersection of the rank constraints.
   ///
   /// \pre `EqualOrUnspecified(a, b)` or `EqualOrUnspecified(constraints)`
+  ///
+  /// \membergroup Composition
   static constexpr RankConstraint And(DimensionIndex a, DimensionIndex b) {
     assert(EqualOrUnspecified(a, b));
     return RankConstraint(a == dynamic_rank ? b : a);
@@ -135,6 +156,8 @@ struct RankConstraint {
   ///   `dynamic_rank`.
   ///
   /// - Otherwise, the result is the sum of the two fixed ranks.
+  ///
+  /// \membergroup Composition
   static constexpr RankConstraint Add(DimensionIndex a, DimensionIndex b) {
     if (a == dynamic_rank || b == dynamic_rank) return dynamic_rank;
     return RankConstraint(a + b);
@@ -156,6 +179,8 @@ struct RankConstraint {
   /// - Otherwise, the result is equal to the difference of the two fixed ranks.
   ///
   /// \pre `GreaterEqualOrUnspecified(a, b)`
+  ///
+  /// \membergroup Composition
   static constexpr RankConstraint Subtract(DimensionIndex a, DimensionIndex b) {
     if (a == dynamic_rank || b == dynamic_rank) return dynamic_rank;
     assert(a >= b);
@@ -163,11 +188,15 @@ struct RankConstraint {
   }
 
   /// Returns `true` if any rank satisfying `inner` also satisfies `outer`.
+  ///
+  /// \membergroup Comparison
   static constexpr bool Implies(DimensionIndex inner, DimensionIndex outer) {
     return outer == dynamic_rank || outer == inner;
   }
 
   /// Returns `true` if there is at least one rank satisfying all constraints.
+  ///
+  /// \membergroup Comparison
   static constexpr bool EqualOrUnspecified(DimensionIndex a, DimensionIndex b) {
     return a == dynamic_rank || b == dynamic_rank || a == b;
   }
@@ -186,12 +215,16 @@ struct RankConstraint {
 
   /// Returns `true` if some rank satisfying `a` is less than some rank
   /// satisfying `b`.
+  ///
+  /// \membergroup Comparison
   static constexpr bool LessOrUnspecified(DimensionIndex a, DimensionIndex b) {
     return a == dynamic_rank || b == dynamic_rank || a < b;
   }
 
   /// Returns `true` if some rank satisfying `a` is less than or equal to some
   /// rank satisfying `b`.
+  ///
+  /// \membergroup Comparison
   static constexpr bool LessEqualOrUnspecified(DimensionIndex a,
                                                DimensionIndex b) {
     return a == dynamic_rank || b == dynamic_rank || a <= b;
@@ -199,6 +232,8 @@ struct RankConstraint {
 
   /// Returns `true` if some rank satisfying `a` is greater than some rank
   /// satisfying `b`.
+  ///
+  /// \membergroup Comparison
   static constexpr bool GreaterOrUnspecified(DimensionIndex a,
                                              DimensionIndex b) {
     return a == dynamic_rank || b == dynamic_rank || a > b;
@@ -206,27 +241,33 @@ struct RankConstraint {
 
   /// Returns `true` if some rank satisfying `a` is greater than or equal to
   /// some rank satisfying `b`.
+  ///
+  /// \membergroup Comparison
   static constexpr bool GreaterEqualOrUnspecified(DimensionIndex a,
                                                   DimensionIndex b) {
     return a == dynamic_rank || b == dynamic_rank || a >= b;
   }
 };
 
-/// Checks if `rank_spec` is a valid compile-time rank constraint.
+/// Checks if `inline_rank` is a valid compile-time rank constraint.
 ///
 /// Supported values are:
 ///
-/// - Any fixed rank for `0 <= rank_spec <= kMaxRank`.
+/// - Any fixed rank for `0 <= inline_rank <= kMaxRank`.
 ///
 /// - `dynamic_rank`, indicating an unspecified rank and no inline buffer.
 ///
-/// - :cpp:`dynamic_rank(k)`, for :cpp:`0 <= k <= kMaxRank`, indicating an
-///   unspecified rank with inline storage up to :cpp:`k`.
+/// - ``dynamic_rank(k)``, for ``0 <= k <= kMaxRank``, indicating an
+///   unspecified rank with inline storage up to ``k``.
+///
+/// \relates InlineRank
 constexpr inline bool IsValidInlineRank(InlineRank inline_rank) {
   return inline_rank >= (-kMaxRank - 1) && inline_rank <= kMaxRank;
 }
 
 /// Returns the inline rank limit of a rank spec.
+///
+/// \relates InlineRank
 constexpr inline DimensionIndex InlineRankLimit(InlineRank rank_spec) {
   return (rank_spec <= -1) ? -1 - rank_spec : 0;
 }
@@ -238,17 +279,22 @@ constexpr inline DimensionIndex InlineRankLimit(InlineRank rank_spec) {
 /// `Rank == dynamic_rank`).
 ///
 /// If this is used to specify a default value for a function parameter, e.g.:
-/// `StaticOrDynamicRank<Rank> rank = StaticRank<Rank>()`, the effect is that
-/// the parameter is required if `Rank == dynamic_rank`, but optional otherwise.
+/// ``StaticOrDynamicRank<Rank> rank = StaticRank<Rank>()``, the effect is
+/// that the parameter is required if `Rank == dynamic_rank`, but optional
+/// otherwise.
+///
+/// \relates DimensionIndex
 template <DimensionIndex Rank>
 using StaticRank =
-    std::enable_if_t<Rank >= 0, std::integral_constant<DimensionIndex, Rank>>;
+    std::enable_if_t<(Rank >= 0), std::integral_constant<DimensionIndex, Rank>>;
 
 /// Template alias that evaluates to the type used for representing a static or
 /// dynamic rank.
 ///
 /// If `Rank == dynamic_rank`, this is `DimensionIndex`.  Otherwise, it is
 /// `std::integral_constant<DimensionIndex, Rank>`.
+///
+/// \relates DimensionIndex
 template <DimensionIndex Rank>
 using StaticOrDynamicRank =
     std::conditional_t<(Rank <= dynamic_rank), DimensionIndex,
@@ -256,34 +302,29 @@ using StaticOrDynamicRank =
 
 /// If `Rank == dynamic_rank`, returns `dynamic_rank`.  Otherwise, returns
 /// `StaticRank<Rank>{}`.
+///
+/// \relates RankConstraint
 template <DimensionIndex Rank>
 inline constexpr StaticOrDynamicRank<Rank> GetDefaultRank() {
-  return {};
-}
-
-template <>
-inline constexpr StaticOrDynamicRank<dynamic_rank>
-GetDefaultRank<dynamic_rank>() {
-  return dynamic_rank;
+  if constexpr (Rank == dynamic_rank) {
+    return dynamic_rank;
+  } else {
+    return {};
+  }
 }
 
 /// Evaluates to a type similar to `SourceRef` but with a static rank of
 /// `TargetRank`.
 ///
-/// The actual type is determined by the `RebindRank` template alias defined by
-/// the `StaticCastTraits` specialization for `SourceRef`, which must be of the
-/// following form:
+/// Supported types include ``StaticOrDynamicRank<Rank>``
+/// (i.e. `DimensionIndex` and
+/// ``std::integral_constant<DimensionIndex, Rank>``), `Array`,
+/// `StridedLayout`, `Box`, `TransformedArray`, `TensorStore`.
 ///
-///     template <DimensionIndex TargetRank>
-///     using RebindRank = ...;
-///
-/// Supported types include `StaticOrDynamicRank<Rank>` (i.e. `DimensionIndex`
-/// and `std::integral_constant<std::ptrdiff_t, Rank>`), `Array`,
-/// `StridedLayout`, `Box`, `TransformedArray`, `Spec`, `TensorStore`.
-///
-/// \tparam SourceRef Optionally `const`- and/or reference-qualified source
+/// \tparam SourceRef Optionally ``const``- and/or reference-qualified source
 ///     type.  Any qualifiers are ignored.
 /// \tparam TargetRank Target rank value.
+/// \ingroup compile-time-constraints
 template <typename SourceRef, DimensionIndex TargetRank>
 using RebindRank =
     typename StaticCastTraitsType<SourceRef>::template RebindRank<TargetRank>;
@@ -298,7 +339,7 @@ using RebindRank =
 ///
 /// The semantics of the `Checking` parameter are the same as for `StaticCast`.
 ///
-/// Example:
+/// Example::
 ///
 ///     StaticRank<3> r_static = StaticRankCast<3,
 ///     unchecked>(DimensionIndex(3)); Box<> box = ...; Result<Box<3>>
@@ -308,6 +349,8 @@ using RebindRank =
 /// \tparam TargetRank Target rank value.
 /// \tparam Checking Specifies whether the cast is checked or unchecked.
 /// \param source Source value.
+///
+/// \ingroup compile-time-constraints
 template <DimensionIndex TargetRank,
           CastChecking Checking = CastChecking::checked, typename SourceRef>
 StaticCastResultType<RebindRank<SourceRef, TargetRank>, SourceRef, Checking>
@@ -316,8 +359,8 @@ StaticRankCast(SourceRef&& source) {
       std::forward<SourceRef>(source));
 }
 
-/// Specialization of `StaticCastTraits` for `DimensionIndex` (which is assumed
-/// to represent a rank value).
+// Specialization of `StaticCastTraits` for `DimensionIndex` (which is assumed
+// to represent a rank value).
 template <>
 struct StaticCastTraits<DimensionIndex>
     : public DefaultStaticCastTraits<DimensionIndex> {
@@ -347,9 +390,9 @@ namespace internal_rank {
 std::string DescribeStaticRank(DimensionIndex rank);
 }  // namespace internal_rank
 
-/// Specialization of `StaticCastTraits` for
-/// `std::integral_constant<DimensionIndex, Rank>` (which is assumed to
-/// represent a rank value).
+// Specialization of `StaticCastTraits` for
+// `std::integral_constant<DimensionIndex, Rank>` (which is assumed to represent
+// a rank value).
 template <DimensionIndex Rank>
 struct StaticCastTraits<std::integral_constant<DimensionIndex, Rank>> {
   static constexpr StaticRank<Rank> Construct(StaticRank<Rank>) { return {}; }
@@ -371,6 +414,7 @@ struct StaticCastTraits<std::integral_constant<DimensionIndex, Rank>> {
 /// Validates that `0 <= rank <= kMaxRank`.
 ///
 /// \error `absl::StatusCode::kInvalidArgument` if `rank` is not valid.
+/// \relates DimensionIndex
 absl::Status ValidateRank(DimensionIndex rank);
 
 }  // namespace tensorstore
