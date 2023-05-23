11b7a6a3821427e102aeacde139492bb2de4722a
Laramie Leavitt <lar@google.com>
2023-05-22 17:51:13 -0700

    Extend SHA256Digester to accept absl::Cord
    Related: https://github.com/google/tensorstore/pull/91
    PiperOrigin-RevId: 534240704
    Change-Id: I2caaefd742a7da12e0dddb2ffc27eef866bc5577

--- tensorstore/internal/digest/sha256.cc
+++ tensorstore/internal/digest/sha256.cc
@@ -12,41 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef TENSORSTORE_INTERNAL_DIGEST_SHA256_H_
-#define TENSORSTORE_INTERNAL_DIGEST_SHA256_H_
+#include "tensorstore/internal/digest/sha256.h"
 
-#include <array>
 #include <string_view>
 
 #include "absl/strings/cord.h"
-#include <openssl/sha.h>
 
 namespace tensorstore {
 namespace internal {
 
-/// Riegeli-compatible SHA256 digester.
-class SHA256Digester {
- public:
-  SHA256Digester() { SHA256_Init(&ctx_); }
-
-  void Write(std::string_view src) {
-    SHA256_Update(&ctx_, src.data(), src.size());
+void SHA256Digester::Write(const absl::Cord& cord) {
+  for (std::string_view chunk : cord.Chunks()) {
+    Write(chunk);
   }
-  void Write(const absl::Cord& cord);
-
-  using DigestType = std::array<uint8_t, SHA256_DIGEST_LENGTH>;
-
-  DigestType Digest() {
-    DigestType digest;
-    SHA256_Final(digest.data(), &ctx_);
-    return digest;
-  }
-
- private:
-  SHA256_CTX ctx_;
-};
+}
 
 }  // namespace internal
 }  // namespace tensorstore
-
-#endif  // TENSORSTORE_INTERNAL_DIGEST_SHA256_H_
