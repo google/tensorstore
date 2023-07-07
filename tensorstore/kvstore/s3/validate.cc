6865bbfc7165052850a396192df915cf886c2711
Simon Perkins <simon.perkins@gmail.com>
2023-07-07 10:32:51 +0200

    Initialise static LazyRE2 in ClassifyBucketName

--- tensorstore/kvstore/s3/validate.cc
+++ tensorstore/kvstore/s3/validate.cc
@@ -24,18 +24,18 @@
 namespace tensorstore {
 namespace internal_storage_s3 {
 
-RE2 kIpAddress("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$");
-RE2 kS3BucketValidChars{"^[a-z0-9\\.-]+$"};         // Current
-RE2 kOldS3BucketValidChars{"^[a-zA-Z0-9\\.-_]+$"};  // Old us-east-1 style
-
 // Returns whether the bucket name is valid.
 // https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
 BucketNameType ClassifyBucketName(std::string_view bucket) {
   if (bucket.size() < 3 || bucket.size() > 255) return BucketNameType::Invalid;
   bool old_us_east = bucket.size() > 63;
 
+  static LazyRE2 kIpAddress = {"^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$"};
+  static LazyRE2 kS3BucketValidChars = {"^[a-z0-9\\.-]+$"};         // Current
+  static LazyRE2 kOldS3BucketValidChars = {"^[a-zA-Z0-9\\.-_]+$"};  // Old us-east-1 style
+
   // No IP Address styles
-  if(RE2::FullMatch(bucket, kIpAddress)) return BucketNameType::Invalid;
+  if(RE2::FullMatch(bucket, *kIpAddress)) return BucketNameType::Invalid;
   if(absl::StartsWith(bucket, "xn--")) return BucketNameType::Invalid;
   // reserved for access point alias names
   if(absl::EndsWith(bucket, "-s3alias")) return BucketNameType::Invalid;
@@ -56,7 +56,8 @@ BucketNameType ClassifyBucketName(std::string_view bucket) {
       return BucketNameType::Invalid;
     }
 
-    if(old_us_east ? !RE2::FullMatch(v, kOldS3BucketValidChars) : !RE2::FullMatch(v, kS3BucketValidChars)) {
+
+    if(old_us_east ? !RE2::FullMatch(v, *kOldS3BucketValidChars) : !RE2::FullMatch(v, *kS3BucketValidChars)) {
       return BucketNameType::Invalid;
     }
   }
