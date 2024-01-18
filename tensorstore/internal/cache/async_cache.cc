google/tensorstore

commit a5ca2695432c5ca4d6f9cfa409bcb94fecfb1655

Fix bugs in kvs_backed_cache / kvstore transaction mechanism
- Previously, if within a single transaction, the same key was written
by one ChunkCache entry and then read (but not modified) by another
ChunkCache entry, then write would be lost.

- Previously, if one cache entry performed a transactional read that
was handled by a writeback request to a prior cache entry for the same
key in the same transaction, and an `if_not_equal` condition was
specified and not satisfied, a `set_cancel` callback that was supposed
to be unreachable would incorrectly be invoked, leading to a crash.

PiperOrigin-RevId: 595784166
Change-Id: Ie060614623c9541a31934e037bc757dab5e53695

tensorstore/internal/cache/async_cache.h
@@ -753,16 +753,34 @@ class AsyncCache : public Cache {
     using ApplyReceiver = AnyReceiver<absl::Status, ReadState>;
 
     struct ApplyOptions {
-      /// Returned `ReadStateUpdate` must reflect an existing read state that is
-      /// current as of `staleness_bound`.
+      // Returned `ReadStateUpdate` must reflect an existing read state that is
+      // current as of `staleness_bound`.
       absl::Time staleness_bound;
 
-      /// If `true`, the `data` returned in the `ReadState` will be ignored.
-      /// This option is used if `DoApply` is called solely to validate the read
-      /// state.  If no validation is necessary, the `stamp` field of the
-      /// `ReadState` may be set to
-      /// `TimestampedStorageGeneration::Unconditional()`.
-      bool validate_only = false;
+      enum ApplyMode {
+        // The `stamp` field of the `ReadState` may be set to
+        // `TimestampedStorageGeneration::Unconditional()` to indicate that this
+        // transaction node does nothing (e.g. read-only and does not
+        // validation).  In this case the `data` returned in the `ReadState`
+        // will be ignored.  Otherwise, the `stamp` must not be
+        // `StorageGeneration::Unknown()`, and should be marked "dirty" if the
+        // data has been modified by this transaction node.
+        kNormal,
+
+        // The `data` returned in the `ReadState` must be valid even if this
+        // transaction node does not modify it.  The `stamp` field of the
+        // `ReadState` must not be
+        // `TimestampedStorageGeneration::Unconditional()`.
+        kSpecifyUnchanged,
+
+        // The `data` returned in the `ReadState` will be ignored.  This option
+        // is used if `DoApply` is called solely to validate the read state.  If
+        // no validation is necessary, the `stamp` field of the `ReadState` may
+        // be set to `TimestampedStorageGeneration::Unconditional()`.
+        kValidateOnly,
+      };
+
+      ApplyMode apply_mode = kNormal;
     };
 
     /// Requests an updated read state that reflects the modifications made by
@@ -778,6 +796,8 @@ class AsyncCache : public Cache {
     /// This is not invoked directly by `AsyncCache` (and therefore does not
     /// necessarily need to be implemented), but is required by
     /// `KvsBackedCache`.
+    ///
+    /// Must not call `set_cancel`.
     virtual void DoApply(ApplyOptions option, ApplyReceiver receiver);
 
     /// Invoked by the `TransactionState` implementation to commit this node.

tensorstore/internal/cache/kvs_backed_cache.h
@@ -198,7 +198,7 @@ class KvsBackedCache : public Parent {
     virtual void DoEncode(
         std::shared_ptr<const typename Derived::ReadData> read_data,
         EncodeReceiver receiver) {
-      ABSL_UNREACHABLE();  // COV_NF_LINE;
+      ABSL_UNREACHABLE();  // COV_NF_LINE
     }
 
     absl::Status AnnotateError(const absl::Status& error, bool reading) {
@@ -303,7 +303,9 @@ class KvsBackedCache : public Parent {
         void set_value(AsyncCache::ReadState update) {
           if (!StorageGeneration::NotEqualOrUnspecified(update.stamp.generation,
                                                         if_not_equal_)) {
-            return execution::set_cancel(receiver_);
+            return execution::set_value(
+                receiver_,
+                kvstore::ReadResult::Unspecified(std::move(update.stamp)));
           }
           if (!StorageGeneration::IsInnerLayerDirty(update.stamp.generation) &&
               writeback_mode_ !=
@@ -312,7 +314,8 @@ class KvsBackedCache : public Parent {
               self_->new_data_ = std::move(update.data);
             }
             return execution::set_value(
-                receiver_, kvstore::ReadResult::Unspecified(update.stamp));
+                receiver_,
+                kvstore::ReadResult::Unspecified(std::move(update.stamp)));
           }
           ABSL_LOG_IF(INFO, TENSORSTORE_ASYNC_CACHE_DEBUG)
               << *self_ << "DoEncode";
@@ -327,8 +330,20 @@ class KvsBackedCache : public Parent {
       };
       AsyncCache::TransactionNode::ApplyOptions apply_options;
       apply_options.staleness_bound = options.staleness_bound;
-      apply_options.validate_only =
-          options.writeback_mode == ReadModifyWriteSource::kValidateOnly;
+      switch (options.writeback_mode) {
+        case ReadModifyWriteSource::kValidateOnly:
+          apply_options.apply_mode =
+              AsyncCache::TransactionNode::ApplyOptions::kValidateOnly;
+          break;
+        case ReadModifyWriteSource::kSpecifyUnchangedWriteback:
+          apply_options.apply_mode =
+              AsyncCache::TransactionNode::ApplyOptions::kSpecifyUnchanged;
+          break;
+        case ReadModifyWriteSource::kNormalWriteback:
+          apply_options.apply_mode =
+              AsyncCache::TransactionNode::ApplyOptions::kNormal;
+          break;
+      }
       this->DoApply(
           std::move(apply_options),
           ApplyReceiverImpl{this, std::move(options.if_not_equal),
@@ -348,6 +363,8 @@ class KvsBackedCache : public Parent {
 
     // Target to which this `ReadModifyWriteSource` is bound.
     ReadModifyWriteTarget* target_;
+
+    // New data for the cache if the writeback completes successfully.
     std::shared_ptr<const void> new_data_;
   };
 

tensorstore/internal/cache/chunk_cache.cc
@@ -557,33 +557,29 @@ absl::Status ChunkCache::TransactionNode::OnModified() {
   return absl::OkStatus();
 }
 
-namespace {
-bool IsCommitUnconditional(ChunkCache::TransactionNode& node) {
-  return node.IsUnconditional() || !node.is_modified;
-}
-}  // namespace
-
 void ChunkCache::TransactionNode::DoApply(ApplyOptions options,
                                           ApplyReceiver receiver) {
-  if (options.validate_only) {
+  if (options.apply_mode == ApplyOptions::kValidateOnly) {
     execution::set_value(
         receiver, ReadState{{}, TimestampedStorageGeneration::Unconditional()});
     return;
   }
   auto continuation = WithExecutor(
       GetOwningCache(*this).executor(),
-      [this, receiver = std::move(receiver)](
+      [this, receiver = std::move(receiver),
+       specify_unchanged =
+           options.apply_mode == ApplyOptions::kSpecifyUnchanged](
           tensorstore::ReadyFuture<const void> future) mutable {
         if (!future.result().ok()) {
           return execution::set_error(receiver, future.result().status());
         }
         AsyncCache::ReadState read_state;
-        if (!IsCommitUnconditional(*this)) {
-          read_state = AsyncCache::ReadLock<void>(*this).read_state();
-        } else {
+        if (this->IsUnconditional() ||
+            (!this->is_modified && !specify_unchanged)) {
           read_state.stamp = TimestampedStorageGeneration::Unconditional();
+        } else {
+          read_state = AsyncCache::ReadLock<void>(*this).read_state();
         }
-        std::shared_ptr<const void> new_data;
         if (is_modified) {
           // Protect against concurrent calls to `DoApply`, since this may
           // modify the write arrays to incorporate the read state.
@@ -595,7 +591,9 @@ void ChunkCache::TransactionNode::DoApply(ApplyOptions options,
         }
         execution::set_value(receiver, std::move(read_state));
       });
-  if (IsCommitUnconditional(*this)) {
+  if (this->IsUnconditional() ||
+      (!this->is_modified &&
+       options.apply_mode != ApplyOptions::kSpecifyUnchanged)) {
     continuation(MakeReadyFuture());
   } else {
     this->Read(options.staleness_bound)

tensorstore/internal/cache/chunk_cache_test.cc
@@ -1595,4 +1595,131 @@ TEST_F(ChunkCacheTest, SelfCopySameChunkSeparateCachesWithExistingData) {
   EXPECT_THAT(GetChunk({0}), ElementsAre(MakeArray<int>({42, 42})));
 }
 
+TEST_F(ChunkCacheTest, SeparateCachesTransactionalReadThenWrite) {
+  // Dimension 0 is chunked with a size of 2.
+  grid = ChunkGridSpecification({ChunkGridSpecification::Component{
+      SharedArray<const void>(MakeArray<int>({1, 2})), Box<>(1)}});
+  auto cache1 = MakeChunkCache();
+  auto cache2 = MakeChunkCache();
+
+  Transaction transaction(tensorstore::isolated);
+
+  auto read_future =
+      tensorstore::Read(GetTensorStore(cache1, {}, 0, transaction) |
+                        tensorstore::Dims(0).TranslateSizedInterval(0, 2));
+  {
+    auto r = mock_store->read_requests.pop();
+    EXPECT_THAT(ParseKey(r.key), ElementsAre(0));
+    r(memory_store);
+  }
+
+  EXPECT_THAT(read_future.result(),
+              ::testing::Optional(MakeArray<int>({1, 2})));
+
+  TENSORSTORE_ASSERT_OK(tensorstore::Write(
+      MakeArray<int>({42, 43}),
+      GetTensorStore(cache2, {}, 0, transaction) |
+          tensorstore::Dims(0).TranslateSizedInterval(0, 2)));
+
+  transaction.CommitAsync().IgnoreFuture();
+
+  {
+    auto r = mock_store->write_requests.pop();
+    EXPECT_THAT(ParseKey(r.key), ElementsAre(0));
+    r(memory_store);
+  }
+
+  TENSORSTORE_EXPECT_OK(transaction.future());
+
+  EXPECT_THAT(GetChunk({0}), ElementsAre(MakeArray<int>({42, 43})));
+}
+
+TEST_F(ChunkCacheTest, SeparateCachesTransactionalWriteThenRead) {
+  // Dimension 0 is chunked with a size of 2.
+  grid = ChunkGridSpecification({ChunkGridSpecification::Component{
+      SharedArray<const void>(MakeArray<int>({1, 2})), Box<>(1)}});
+  auto cache1 = MakeChunkCache();
+  auto cache2 = MakeChunkCache();
+
+  Transaction transaction(tensorstore::isolated);
+
+  TENSORSTORE_ASSERT_OK(tensorstore::Write(
+      MakeArray<int>({42, 43}),
+      GetTensorStore(cache1, {}, 0, transaction) |
+          tensorstore::Dims(0).TranslateSizedInterval(0, 2)));
+
+  EXPECT_THAT(
+      tensorstore::Read(GetTensorStore(cache2, {}, 0, transaction) |
+                        tensorstore::Dims(0).TranslateSizedInterval(0, 2))
+          .result(),
+      ::testing::Optional(MakeArray<int>({42, 43})));
+
+  transaction.CommitAsync().IgnoreFuture();
+
+  {
+    auto r = mock_store->write_requests.pop();
+    EXPECT_THAT(ParseKey(r.key), ElementsAre(0));
+    r(memory_store);
+  }
+
+  TENSORSTORE_EXPECT_OK(transaction.future());
+
+  EXPECT_THAT(GetChunk({0}), ElementsAre(MakeArray<int>({42, 43})));
+}
+
+TEST_F(ChunkCacheTest, SeparateCachesReadIfNotEqualAbort) {
+  // Dimension 0 is chunked with a size of 2.
+  grid = ChunkGridSpecification({ChunkGridSpecification::Component{
+      SharedArray<const void>(MakeArray<int>({1, 2})), Box<>(1)}});
+  auto cache1 = MakeChunkCache();
+  auto cache2 = MakeChunkCache();
+
+  Transaction transaction(tensorstore::isolated);
+
+  // Read to populate entry in cache1.
+  {
+    auto read_future =
+        tensorstore::Read(GetTensorStore(cache1, {}, 0, transaction) |
+                          tensorstore::Dims(0).TranslateSizedInterval(0, 2));
+    {
+      auto r = mock_store->read_requests.pop();
+      EXPECT_THAT(ParseKey(r.key), ElementsAre(0));
+      EXPECT_EQ(StorageGeneration::Unknown(), r.options.if_not_equal);
+      r(memory_store);
+    }
+    EXPECT_THAT(read_future.result(),
+                ::testing::Optional(MakeArray<int>({1, 2})));
+  }
+
+  // Read to populate read state of transaction node in cache2.
+  {
+    auto read_future =
+        tensorstore::Read(GetTensorStore(cache2, {}, 0, transaction) |
+                          tensorstore::Dims(0).TranslateSizedInterval(0, 2));
+    {
+      auto r = mock_store->read_requests.pop();
+      EXPECT_THAT(ParseKey(r.key), ElementsAre(0));
+      EXPECT_EQ(StorageGeneration::NoValue(), r.options.if_not_equal);
+      r(memory_store);
+    }
+    EXPECT_THAT(read_future.result(),
+                ::testing::Optional(MakeArray<int>({1, 2})));
+  }
+
+  // Re-read transaction node in cache2.
+  {
+    auto read_future =
+        tensorstore::Read(GetTensorStore(cache2, {}, 0, transaction) |
+                          tensorstore::Dims(0).TranslateSizedInterval(0, 2));
+    {
+      auto r = mock_store->read_requests.pop();
+      EXPECT_THAT(ParseKey(r.key), ElementsAre(0));
+      EXPECT_EQ(StorageGeneration::NoValue(), r.options.if_not_equal);
+      r(memory_store);
+    }
+    EXPECT_THAT(read_future.result(),
+                ::testing::Optional(MakeArray<int>({1, 2})));
+  }
+}
+
 }  // namespace

tensorstore/internal/cache/kvs_backed_cache_testutil.cc
@@ -131,7 +131,7 @@ Future<absl::Cord> KvsBackedTestCache::Entry::ReadValue(
 
 void KvsBackedTestCache::TransactionNode::DoApply(ApplyOptions options,
                                                   ApplyReceiver receiver) {
-  if (options.validate_only && validators.empty()) {
+  if (options.apply_mode == ApplyOptions::kValidateOnly && validators.empty()) {
     execution::set_value(
         receiver, ReadState{{}, TimestampedStorageGeneration::Unconditional()});
     return;

tensorstore/kvstore/read_modify_write.h
@@ -142,6 +142,11 @@ class ReadModifyWriteSource {
   /// This is called by the `ReadModifyWriteTarget` either during commit, or
   /// in response to a read request by a subsequent read-modify-write
   /// operation layered on top of this operation.
+  ///
+  /// If `options.if_not_equal` is not satisfied, invokes `set_value` on
+  /// `receiver` with a `ReadResult` with a state of `ReadResult::kUnspecified`.
+  ///
+  /// Must not invoke `set_cancel` on `receiver`.
   virtual void KvsWriteback(WritebackOptions options,
                             WritebackReceiver receiver) = 0;
 

commit 70b16aeb3f8ff8d7de7de9bb03faadb585f7b877

kvs_backed_cache: Avoid no-op commit corrupting the cache
The recent commit
https://github.com/google/tensorstore/commit/a5ca2695432c5ca4d6f9cfa409bcb94fecfb1655
introduced a bug whereby committing an AsyncCache::TransactionNode
that leaves the value unmodified could result in:
- the timestamp unnecessarily being set to `absl::InfinitePast()`,
  leading to unnecessary re-reads; or
- the data value incorrectly being set to nullptr, leading to
  incorrect read results.

PiperOrigin-RevId: 599336761
Change-Id: I75b6e91db691b48e8821ce172b7551cbcf1c2b69

tensorstore/internal/cache/BUILD
@@ -104,6 +104,10 @@ tensorstore_cc_library(
     name = "kvs_backed_cache",
     srcs = ["kvs_backed_cache.cc"],
     hdrs = ["kvs_backed_cache.h"],
+    defines = select({
+        ":async_cache_debug_setting": ["TENSORSTORE_ASYNC_CACHE_DEBUG"],
+        "//conditions:default": [],
+    }),
     deps = [
         ":async_cache",
         "//tensorstore:transaction",

tensorstore/internal/cache/async_cache.cc
@@ -50,6 +50,10 @@ using PendingWritebackQueueAccessor =
     TransactionNode::PendingWritebackQueueAccessor;
 using PrepareForCommitState = TransactionNode::PrepareForCommitState;
 
+// While the real epsilon is `absl::Nanoseconds(1) / 4`, `operator/` is not
+// constexpr, and this value is sufficient for use here.
+constexpr absl::Duration kEpsilonDuration = absl::Nanoseconds(1);
+
 void AcquireReadRequestReference(Entry& entry) {
   // Prevent the entry from being destroyed while the read is in progress.
   internal::PinnedCacheEntry<AsyncCache>(&entry).release();
@@ -229,6 +233,7 @@ void SetReadState(EntryOrNode& entry_or_node, ReadState&& read_state,
       return;
     }
   }
+  entry_or_node.read_request_state_.known_to_be_stale = false;
   entry_or_node.read_request_state_.read_state = std::move(read_state);
   size_t change =
       read_state_size -
@@ -245,21 +250,27 @@ void SetReadState(EntryOrNode& entry_or_node, ReadState&& read_state,
 
 template <typename EntryOrNode>
 Future<const void> RequestRead(EntryOrNode& entry_or_node,
-                               absl::Time staleness_bound) {
+                               absl::Time staleness_bound,
+                               bool must_not_be_known_to_be_stale) {
   static_assert(std::is_same_v<EntryOrNode, Entry> ||
                 std::is_same_v<EntryOrNode, TransactionNode>);
   auto& entry = GetOwningEntry(entry_or_node);
   UniqueWriterLock lock(entry);
 
-  auto& request_state = entry_or_node.read_request_state_;
-  const auto existing_time =
-      GetEffectiveReadRequestState(entry_or_node).read_state.stamp.time;
+  auto& effective_request_state = GetEffectiveReadRequestState(entry_or_node);
+  const auto existing_time = effective_request_state.read_state.stamp.time;
   if (existing_time != absl::InfinitePast() &&
       existing_time >= staleness_bound) {
-    // `staleness_bound` satisfied by current data.
-    return MakeReadyFuture();
+    if (must_not_be_known_to_be_stale &&
+        effective_request_state.known_to_be_stale) {
+      staleness_bound = existing_time + kEpsilonDuration;
+    } else {
+      // `staleness_bound` satisfied by current data.
+      return MakeReadyFuture();
+    }
   }
 
+  auto& request_state = entry_or_node.read_request_state_;
   // `staleness_bound` not satisfied by current data.
   request_state.queued_time = std::max(request_state.queued_time,
                                        std::min(staleness_bound, absl::Now()));
@@ -423,10 +434,12 @@ size_t AsyncCache::DoGetSizeInBytes(Cache::Entry* base_entry) {
          entry->read_request_state_.read_state_size;
 }
 
-Future<const void> AsyncCache::Entry::Read(absl::Time staleness_bound) {
+Future<const void> AsyncCache::Entry::Read(absl::Time staleness_bound,
+                                           bool must_not_be_known_to_be_stale) {
   ABSL_LOG_IF(INFO, TENSORSTORE_ASYNC_CACHE_DEBUG)
-      << *this << "Read: staleness_bound=" << staleness_bound;
-  return RequestRead(*this, staleness_bound);
+      << *this << "Read: staleness_bound=" << staleness_bound
+      << ", must_not_be_known_to_be_stale=" << must_not_be_known_to_be_stale;
+  return RequestRead(*this, staleness_bound, must_not_be_known_to_be_stale);
 }
 
 void AsyncCache::Entry::ReadSuccess(ReadState&& read_state) {
@@ -448,15 +461,17 @@ AsyncCache::TransactionNode::TransactionNode(Entry& entry)
       size_updated_(false) {}
 
 Future<const void> AsyncCache::TransactionNode::Read(
-    absl::Time staleness_bound) {
+    absl::Time staleness_bound, bool must_not_be_known_to_be_stale) {
   ABSL_LOG_IF(INFO, TENSORSTORE_ASYNC_CACHE_DEBUG)
-      << *this << "Read: staleness_bound=" << staleness_bound;
+      << *this << "Read: staleness_bound=" << staleness_bound
+      << ", must_not_be_known_to_be_stale=" << must_not_be_known_to_be_stale;
   if (reads_committed_ &&
       (prepare_for_commit_state_.load(std::memory_order_acquire) !=
        PrepareForCommitState::kReadyForCommitCalled)) {
-    return RequestRead(GetOwningEntry(*this), staleness_bound);
+    return RequestRead(GetOwningEntry(*this), staleness_bound,
+                       must_not_be_known_to_be_stale);
   }
-  return RequestRead(*this, staleness_bound);
+  return RequestRead(*this, staleness_bound, must_not_be_known_to_be_stale);
 }
 
 void AsyncCache::TransactionNode::ReadSuccess(ReadState&& read_state) {
@@ -531,8 +546,7 @@ void AsyncCache::TransactionNode::WritebackSuccess(ReadState&& read_state) {
     assert(read_state_time >= request_state.read_state.stamp.time);
     SetReadState(entry, std::move(read_state), read_state_size);
   } else if (read_state_time > request_state.read_state.stamp.time) {
-    read_state_time = request_state.read_state.stamp.time =
-        absl::InfinitePast();
+    request_state.known_to_be_stale = true;
   }
 
   QueuedReadHandler queued_read_handler(request_state, read_state_time);

tensorstore/internal/cache/async_cache.h
@@ -195,6 +195,10 @@ class AsyncCache : public Cache {
     /// The most recently-cached read state.
     ReadState read_state;
 
+    /// `read_state` is known to be out-of-date, due to a local modification not
+    /// reflected in the cache.
+    bool known_to_be_stale = false;
+
     /// The size in bytes consumed by `read_state.read_data`.  This is the
     /// cached result of calling
     /// `Entry::ComputeReadDataSizeInBytes(read_state.read_data.get())`.
@@ -346,10 +350,15 @@ class AsyncCache : public Cache {
     /// Requests data no older than `staleness_bound`.
     ///
     /// \param staleness_bound Limit on data staleness.
+    /// \param must_not_be_known_to_be_stale Requests newer data if the existing
+    ///     data is known to be out-of-date (e.g. due to a local write not
+    ///     reflected in the cache), even if the existing data satisfies
+    ///     `staleness_bound`.
     /// \returns A future that resolves to a success state once data no older
     ///     than `staleness_bound` is available, or to an error state if the
     ///     request failed.
-    Future<const void> Read(absl::Time staleness_bound);
+    Future<const void> Read(absl::Time staleness_bound,
+                            bool must_not_be_known_to_be_stale = true);
 
     /// Obtains an existing or new transaction node for the specified entry and
     /// transaction.  May also be used to obtain an implicit transaction node.
@@ -616,7 +625,8 @@ class AsyncCache : public Cache {
 
     /// Requests a read state for this transaction node that is current as of
     /// the specified `staleness_bound`.
-    Future<const void> Read(absl::Time staleness_bound);
+    Future<const void> Read(absl::Time staleness_bound,
+                            bool must_not_be_known_to_be_stale = true);
 
     /// Requests initial or updated data from persistent storage for a single
     /// `Entry`.

tensorstore/internal/cache/kvs_backed_cache.h
@@ -284,7 +284,7 @@ class KvsBackedCache : public Parent {
       }
       struct EncodeReceiverImpl {
         TransactionNode* self_;
-        AsyncCache::ReadState update_;
+        TimestampedStorageGeneration update_stamp_;
         ReadModifyWriteSource::WritebackReceiver receiver_;
         void set_error(absl::Status error) {
           error = GetOwningEntry(*self_).AnnotateError(std::move(error),
@@ -295,12 +295,8 @@ class KvsBackedCache : public Parent {
         void set_value(std::optional<absl::Cord> value) {
           kvstore::ReadResult read_result =
               value ? kvstore::ReadResult::Value(std::move(*value),
-                                                 std::move(update_.stamp))
-                    : kvstore::ReadResult::Missing(std::move(update_.stamp));
-
-          // FIXME: only save if committing, also could do this inside
-          // ApplyReceiverImpl
-          self_->new_data_ = std::move(update_.data);
+                                                 std::move(update_stamp_))
+                    : kvstore::ReadResult::Missing(std::move(update_stamp_));
           execution::set_value(receiver_, std::move(read_result));
         }
       };
@@ -341,7 +337,13 @@ class KvsBackedCache : public Parent {
           if (!StorageGeneration::IsInnerLayerDirty(update.stamp.generation) &&
               writeback_mode_ !=
                   ReadModifyWriteSource::kSpecifyUnchangedWriteback) {
-            if (self_->transaction()->commit_started()) {
+            ABSL_LOG_IF(INFO, TENSORSTORE_ASYNC_CACHE_DEBUG)
+                << *self_ << "DoApply: if_not_equal=" << if_not_equal_
+                << ", mode=" << writeback_mode_
+                << ", unmodified: " << update.stamp;
+            if (StorageGeneration::IsUnknown(update.stamp.generation)) {
+              self_->new_data_ = std::nullopt;
+            } else {
               self_->new_data_ = std::move(update.data);
             }
             return execution::set_value(
@@ -349,13 +351,18 @@ class KvsBackedCache : public Parent {
                 kvstore::ReadResult::Unspecified(std::move(update.stamp)));
           }
           ABSL_LOG_IF(INFO, TENSORSTORE_ASYNC_CACHE_DEBUG)
+              << *self_ << "DoApply: if_not_equal=" << if_not_equal_
+              << ", mode=" << writeback_mode_ << ", encoding: " << update.stamp
+              << ", commit_started=" << self_->transaction()->commit_started();
+          self_->new_data_ = update.data;
+          ABSL_LOG_IF(INFO, TENSORSTORE_ASYNC_CACHE_DEBUG)
               << *self_ << "DoEncode";
           auto update_data =
               std::static_pointer_cast<const typename Derived::ReadData>(
-                  update.data);
+                  std::move(update.data));
           GetOwningEntry(*self_).DoEncode(
               std::move(update_data),
-              EncodeReceiverImpl{self_, std::move(update),
+              EncodeReceiverImpl{self_, std::move(update.stamp),
                                  std::move(receiver_)});
         }
       };
@@ -382,8 +389,13 @@ class KvsBackedCache : public Parent {
     }
 
     void KvsWritebackSuccess(TimestampedStorageGeneration new_stamp) override {
-      return this->WritebackSuccess(
-          AsyncCache::ReadState{std::move(new_data_), std::move(new_stamp)});
+      if (new_data_) {
+        this->WritebackSuccess(
+            AsyncCache::ReadState{std::move(*new_data_), std::move(new_stamp)});
+      } else {
+        // Unmodified.
+        this->WritebackSuccess(AsyncCache::ReadState{});
+      }
     }
     void KvsWritebackError() override { this->WritebackError(); }
 
@@ -416,7 +428,7 @@ class KvsBackedCache : public Parent {
     ReadModifyWriteTarget* target_;
 
     // New data for the cache if the writeback completes successfully.
-    std::shared_ptr<const void> new_data_;
+    std::optional<std::shared_ptr<const void>> new_data_;
 
     // If not `StorageGeneration::Unknown()`, requires that the prior generation
     // match this generation when the transaction is committed.