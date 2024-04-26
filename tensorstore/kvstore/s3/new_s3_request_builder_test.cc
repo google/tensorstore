2a08922d44be17700f1ab9d0bcb75fc7544f302c
Simon Perkins <simon.perkins@gmail.com>
2024-04-26 18:00:08 +0200

    Add NewS3RequestBuilder

---
+++ tensorstore/kvstore/s3/new_s3_request_builder_test.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "tensorstore/kvstore/s3/s3_context.h"
+#include "tensorstore/kvstore/s3/new_s3_request_builder.h"
+
+
+using ::tensorstore::internal_kvstore_s3::NewS3RequestBuilder;
+
+namespace {
+
+TEST(NewS3RequestBuilderTest, Basic) {
+  auto builder = NewS3RequestBuilder("get", "http://bucket")
+                  .AddBody(absl::Cord{"foobar"})
+                  .AddHeader("foo: bar")
+                  .AddQueryParameter("qux", "baz");
+
+  auto ctx = tensorstore::internal_kvstore_s3::GetAwsContext();
+
+  auto req = builder.BuildRequest(*ctx);
+  EXPECT_TRUE(builder.request_.HasAuthorization());
+
+  ABSL_LOG(INFO) << req;
+}
+
+}  // namespace
