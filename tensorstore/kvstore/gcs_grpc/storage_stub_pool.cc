1451de27b0d56e14644b6c59ea16d99380de1b56
Laramie Leavitt <lar@google.com>
2024-08-13 11:58:28 -0700

    Update gcs_grpc logging to add more details.
    PiperOrigin-RevId: 662601416
    Change-Id: I7ab88d1831d77f05cba21d7a13a10a2ad4d80484

--- tensorstore/kvstore/gcs_grpc/storage_stub_pool.cc
+++ tensorstore/kvstore/gcs_grpc/storage_stub_pool.cc
@@ -18,12 +18,14 @@
 #include <stdint.h>
 
 #include <algorithm>
+#include <cstddef>
 #include <memory>
 #include <optional>
 #include <string>
 #include <string_view>
 #include <thread>  // NOLINT
 #include <utility>
+#include <vector>
 
 #include "absl/base/attributes.h"
 #include "absl/base/const_init.h"
@@ -40,11 +42,19 @@
 #include "grpcpp/create_channel.h"  // third_party
 #include "grpcpp/security/credentials.h"  // third_party
 #include "grpcpp/support/channel_arguments.h"  // third_party
+#include "grpcpp/support/client_interceptor.h"  // third_party
+#include "grpcpp/support/interceptor.h"  // third_party
+#include "grpcpp/support/status.h"  // third_party
+#include "google/protobuf/message.h"
 #include "tensorstore/internal/env.h"
+#include "tensorstore/internal/grpc/utils.h"
 #include "tensorstore/internal/log/verbose_flag.h"
+#include "tensorstore/proto/proto_util.h"
 
-// protos
+// Protos
+#include "google/protobuf/empty.pb.h"
 #include "google/storage/v2/storage.grpc.pb.h"
+#include "google/storage/v2/storage.pb.h"
 
 ABSL_FLAG(std::optional<bool>, tensorstore_gcs_grpc_use_local_subchannel_pool,
           std::nullopt,
@@ -55,7 +65,17 @@ ABSL_FLAG(std::optional<uint32_t>, tensorstore_gcs_grpc_channels, std::nullopt,
           "Default (and maximum) channels to use in gcs_grpc driver. "
           "Overrides TENSORSTORE_GCS_GRPC_CHANNELS.");
 
+using ::grpc::experimental::ClientInterceptorFactoryInterface;
+using ::grpc::experimental::ClientRpcInfo;
+using ::grpc::experimental::InterceptionHookPoints;
+using ::grpc::experimental::Interceptor;
+using ::grpc::experimental::InterceptorBatchMethods;
 using ::tensorstore::internal::GetFlagOrEnvValue;
+using ::tensorstore::internal::GrpcStatusToAbslStatus;
+
+using ::google::storage::v2::DeleteObjectRequest;
+using ::google::storage::v2::ReadObjectRequest;
+using ::google::storage::v2::WriteObjectRequest;
 
 namespace tensorstore {
 namespace internal_gcs_grpc {
@@ -64,6 +84,108 @@ namespace {
 ABSL_CONST_INIT absl::Mutex global_mu(absl::kConstInit);
 
 ABSL_CONST_INIT internal_log::VerboseFlag gcs_grpc_logging("gcs_grpc");
+
+enum class Method {
+  kReadObject = 0,
+  kWriteObject,
+  kDeleteObject,
+  kListObjects
+};
+
+static const std::string_view kNames[] = {"ReadObject", "WriteObject",
+                                          "DeleteObject", "ListObjects"};
+
+// Implements detailed logging for gRPC calls to GCS.
+class LoggingInterceptor : public Interceptor {
+ public:
+  LoggingInterceptor(Method method, bool verbose)
+      : method_(method), verbose_(verbose) {}
+
+  std::string_view method_name() const {
+    return kNames[static_cast<size_t>(method_)];
+  }
+
+  void Intercept(InterceptorBatchMethods* methods) override {
+    if (methods->QueryInterceptionHookPoint(
+            InterceptionHookPoints::PRE_SEND_MESSAGE)) {
+      auto* message =
+          static_cast<const google::protobuf::Message*>(methods->GetSendMessage());
+      bool is_first_message = !started_;
+      if (!started_) {
+        started_ = true;
+        switch (method_) {
+          case Method::kReadObject:
+            object_name_ =
+                static_cast<const ReadObjectRequest*>(message)->object();
+            break;
+          case Method::kWriteObject:
+            object_name_ = static_cast<const WriteObjectRequest*>(message)
+                               ->write_object_spec()
+                               .resource()
+                               .name();
+            break;
+          case Method::kDeleteObject:
+            object_name_ =
+                static_cast<const DeleteObjectRequest*>(message)->object();
+            break;
+          case Method::kListObjects:
+            break;
+        }
+      }
+      if (verbose_) {
+        ABSL_LOG(INFO) << "Begin: " << method_name() << " " << object_name_
+                       << " " << ConciseDebugString(*message);
+      } else if (is_first_message) {
+        ABSL_LOG(INFO) << "Begin: " << method_name() << " " << object_name_;
+      }
+    }
+    if (verbose_ && methods->QueryInterceptionHookPoint(
+                        InterceptionHookPoints::POST_RECV_MESSAGE)) {
+      ABSL_LOG(INFO) << method_name() << " "
+                     << ConciseDebugString(*static_cast<const google::protobuf::Message*>(
+                            methods->GetRecvMessage()));
+    }
+    if (methods->QueryInterceptionHookPoint(
+            InterceptionHookPoints::POST_RECV_STATUS)) {
+      if (auto* status = methods->GetRecvStatus();
+          status != nullptr && !status->ok()) {
+        ABSL_LOG(INFO) << "Error: " << method_name() << " " << object_name_
+                       << " " << GrpcStatusToAbslStatus(*status);
+      } else {
+        ABSL_LOG(INFO) << "End: " << method_name() << " " << object_name_;
+      }
+    }
+    methods->Proceed();
+  }
+
+ private:
+  const Method method_;
+  const bool verbose_;
+  bool started_ = false;
+  std::string object_name_;
+};
+
+// Creates a logging interceptor.
+class LoggingInterceptorFactory : public ClientInterceptorFactoryInterface {
+ public:
+  Interceptor* CreateClientInterceptor(ClientRpcInfo* info) override {
+    if (!gcs_grpc_logging.Level(1)) {
+      return nullptr;
+    }
+    std::string_view method = info->method();
+    bool verbose = gcs_grpc_logging.Level(2);
+    if (absl::EndsWith(method, "Storage/ReadObject")) {
+      return new LoggingInterceptor(Method::kReadObject, verbose);
+    } else if (absl::EndsWith(method, "Storage/WriteObject")) {
+      return new LoggingInterceptor(Method::kWriteObject, verbose);
+    } else if (absl::EndsWith(method, "Storage/DeleteObject")) {
+      return new LoggingInterceptor(Method::kDeleteObject, verbose);
+    } else if (absl::EndsWith(method, "Storage/ListObjects")) {
+      return new LoggingInterceptor(Method::kListObjects, verbose);
+    }
+    return nullptr;
+  }
+};
 
 /// Returns the number of channels to use.
 /// See also the channel construction in googleapis/google-cloud-cpp repository:
@@ -119,7 +241,15 @@ StorageStubPool::StorageStubPool(
       args.SetInt(GRPC_ARG_CHANNEL_ID, id);
       args.SetInt(GRPC_ARG_DNS_ENABLE_SRV_QUERIES, 0);
     }
-    channels_[id] = grpc::CreateCustomChannel(address_, creds, args);
+
+    // The gRPC interceptor implements detailed logging.
+    std::vector<
+        std::unique_ptr<grpc::experimental::ClientInterceptorFactoryInterface>>
+        interceptors;
+    interceptors.push_back(std::make_unique<LoggingInterceptorFactory>());
+
+    channels_[id] = grpc::experimental::CreateCustomChannelWithInterceptors(
+        address_, creds, args, std::move(interceptors));
     stubs_[id] = Storage::NewStub(channels_[id]);
   }
 }
