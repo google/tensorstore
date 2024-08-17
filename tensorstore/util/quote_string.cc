fa30217309e3fbaa34c7ba1142157e46cedbb109
Laramie Leavitt <lar@google.com>
2024-08-16 19:00:50 -0700

    Prefer StrCat / StrAppend to std::string::operator+
    PiperOrigin-RevId: 663954290
    Change-Id: I5217d40df63b8ee570334d2f15ca2b78721e2056

--- tensorstore/util/quote_string.cc
+++ tensorstore/util/quote_string.cc
@@ -18,11 +18,14 @@
 #include <string_view>
 
 #include "absl/strings/escaping.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
 
 namespace tensorstore {
 
 std::string QuoteString(std::string_view s) {
-  return '"' + absl::CHexEscape(absl::string_view(s.data(), s.size())) + '"';
+  return absl::StrCat(
+      "\"", absl::CHexEscape(absl::string_view(s.data(), s.size())), "\"");
 }
 
 }  // namespace tensorstore
