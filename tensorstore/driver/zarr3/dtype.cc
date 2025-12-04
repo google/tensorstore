9a46c82968fb1e70e1cb14e3b827dcf627b80463
BrianMichell <brianm314@comcast.net>
2025-12-04 17:31:17 +0000

    Fix copyright header date

--- tensorstore/driver/zarr3/dtype.cc
+++ tensorstore/driver/zarr3/dtype.cc
@@ -1,4 +1,4 @@
-// Copyright 2020 The TensorStore Authors
+// Copyright 2025 The TensorStore Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
